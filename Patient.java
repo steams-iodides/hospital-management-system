0337e5cde4a7c93ae4c7cdefa7283e40c209a5f3
Natalia Iv <153074445+steams-iodides@users.noreply.github.com>
2023-12-07 13:13:10 +0700

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,14 @@
+// Patient.java
+public class Patient {
+    private String name;
+    private int age;
+    private String healthCondition;
+
+    public Patient(String name, int age, String healthCondition) {
+        this.name = name;
+        this.age = age;
+        this.healthCondition = healthCondition;
+    }
+
+    // Getters and setters (not shown for brevity)
+}
