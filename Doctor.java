aa743c2daa4879a9bf289ffbaa289ecbe11a9a8c
Natalia Iv <153074445+steams-iodides@users.noreply.github.com>
2023-12-07 13:13:24 +0700

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,12 @@
+// Doctor.java
+public class Doctor {
+    private String name;
+    private String specialization;
+
+    public Doctor(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    // Getters and setters (not shown for brevity)
+}
